2d8ab8dda33226b2e707b9059dd37b640707e02e
gugli4ifenix-design <gugli4ifenix@gmail.com>
2026-04-09 19:45:14 +0300

    fix: Vue Integration

---
+++ packages/vue/src/types.ts
@@ -0,0 +1,15 @@
+import type { AbbyConfig, GetVariantReturn } from "@tryabby/core";
+import type { Readonly, Ref } from "vue";
+
+export interface UseAbbyReturn<T extends AbbyConfig> {
+  variant: Readonly<Ref<GetVariantReturn<T>>>;
+  onAct: (data?: Record<string, unknown>) => void;
+}
+
+export interface UseFeatureFlagReturn {
+  isEnabled: Readonly<Ref<boolean>>;
+}
+
+export interface UseRemoteConfigReturn<T = unknown> {
+  value: Readonly<Ref<T>>;
+}
