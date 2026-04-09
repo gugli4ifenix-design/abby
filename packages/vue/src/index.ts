0edab835d48bdebfda38f17a112aa12e6827a293
gugli4ifenix-design <gugli4ifenix@gmail.com>
2026-04-09 19:45:13 +0300

    fix: Vue Integration

---
+++ packages/vue/src/index.ts
@@ -0,0 +1,8 @@
+export { useAbby } from "./use-abby";
+export { useFeatureFlag } from "./use-feature-flag";
+export { useRemoteConfig } from "./use-remote-config";
+export type {
+  UseAbbyReturn,
+  UseFeatureFlagReturn,
+  UseRemoteConfigReturn,
+} from "./types";
